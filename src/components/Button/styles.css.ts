094c05b4d3672dc39dd7dfc185e7d952278c9fd9
Kajetan August <kajetan.august@gmail.com>
2025-07-18 11:00:06 +0200

    feat(Button): add option for creating button with icon only, add lucide-astro icons

--- src/components/Button/styles.css.ts
+++ src/components/Button/styles.css.ts
@@ -1,5 +1,9 @@
 import {recipe} from '@vanilla-extract/recipes';
 import {theme} from '../../styles/theme.css';
+import {iconVariants, sizeStyles} from "./consts.ts";
+
+
+
 
 export const buttonRecipe = recipe({
     base: {
@@ -74,11 +78,55 @@ export const buttonRecipe = recipe({
                     transform: 'translateY(1px)',
                 },
             },
-            icon: {}
+            iconPrimary: {
+                backgroundColor: theme.colors.luxor_gold,
+                color: theme.colors.neutral_white,
+
+                '&:hover:not(:disabled)': {
+                    backgroundColor: theme.colors.luxor_gold_dark,
+                },
+
+                '&:active:not(:disabled)': {
+                    transform: 'translateY(1px)',
+                },
+            },
+            iconOutline: {
+                border: `1px solid rgba(9, 3, 9, .75)`,
+                backgroundColor: 'transparent',
+                color: theme.colors.neutral_darkest,
+
+                '&:hover:not(:disabled)': {
+                    borderColor: theme.colors.luxor_gold,
+
+                },
+
+                '&:active:not(:disabled)': {
+                    transform: 'translateY(1px)',
+                },
+            },
+            iconGhost: {
+                border: "none",
+                backgroundColor: "transparent",
+                color: theme.colors.neutral_darkest,
+
+                '&:hover:not(:disabled)': {
+                    backgroundColor: theme.colors.neutral_lightest,
+                },
+
+                '&:active:not(:disabled)': {
+                    transform: 'translateY(1px)',
+                },
+            },
         },
         size: {
             small: {padding: '4px 10px'},
             large: {padding: '6px 12px'}
-        }
-    }
+        },
+    },
+    compoundVariants: iconVariants.flatMap(variant =>
+        Object.entries(sizeStyles).map(([size, padding]) => ({
+            variants: { variant, size: size as keyof typeof sizeStyles },
+            style: { padding }
+        }))
+    )
 });
