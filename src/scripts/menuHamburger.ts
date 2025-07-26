d5d07c0c5f36756aec15390ce32302cba9a28112
Kajetan August <kajetan.august@gmail.com>
2025-07-26 13:22:27 +0200

    feat(Header): add breakpoints for Header, add logic for mobile menu button and basic mobile menu

---
+++ src/scripts/menuHamburger.ts
@@ -0,0 +1,16 @@
+import {mobileMenuVisible} from "../sections/Header/styles.css.ts";
+
+const hamburger = document.getElementById('mobile-menu-hamburger');
+const mobileMenu = document.getElementById('mobile-menu');
+const html = document.querySelector('html');
+const body = document.querySelector('body');
+
+hamburger?.addEventListener('click', () => {
+    mobileMenu?.classList.toggle(mobileMenuVisible);
+    const isMenuVisible = mobileMenu?.classList.contains(mobileMenuVisible);
+
+    if (!html || !body) return;
+
+    html.style.overflowY = isMenuVisible ? 'hidden' : 'auto';
+    body.style.overflowY = isMenuVisible ? 'hidden' : 'auto';
+})
