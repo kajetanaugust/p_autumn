f4d93ec2de4e3bd7fb156e6a61b27f0d7696da22
Kajetan August <kajetan.august@gmail.com>
2025-07-28 23:15:27 +0200

    feat(Hero): use optimized astro Image component

--- src/sections/Hero/styles.css.ts
+++ src/sections/Hero/styles.css.ts
@@ -3,3 +3,13 @@ import {style} from "@vanilla-extract/css";
 export const heroCopyElement = style({
     flex: 1,
 })
+
+export const heroImageStyles = style({
+    width: '100%',
+    height: 'auto',
+    display: 'block',
+    maxWidth: '100%',
+    maxHeight: 450,
+    objectFit: 'cover',
+})
+
