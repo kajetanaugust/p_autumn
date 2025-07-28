f438018858825f0b4de106b901c8106053a53d3b
Kajetan August <kajetan.august@gmail.com>
2025-07-28 11:14:26 +0200

    feat(Footer): add basic footer

---
+++ src/sections/Footer/styles.css.ts
@@ -0,0 +1,6 @@
+import {style} from "@vanilla-extract/css";
+import {theme} from "../../styles/theme.css";
+
+export const footerWrapper = style({
+    borderTop: `1px solid ${theme.colors.neutral_lighter}`,
+})
