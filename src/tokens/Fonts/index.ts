cbfdca1cfb0614315088eb1cb6e5d7b3bb409c1a
Kajetan August <kajetan.august@gmail.com>
2025-07-29 11:26:50 +0200

    feat(Fonts): replace Karla with Public Sans font

--- src/tokens/Fonts/index.ts
+++ src/tokens/Fonts/index.ts
@@ -1,6 +1,6 @@
 export const fonts = {
     primary: 'Merriweather, Georgia, "Times New Roman", serif',
-    secondary: 'Karla, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, sans-serif',
+    secondary: 'Public Sans, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, sans-serif',
 } as const;
 
 export type FontFamily = keyof typeof fonts;
