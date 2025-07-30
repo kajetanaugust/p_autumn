e96168fb17bd4326f2d29148bdcb6bc8b55d53e7
Kajetan August <kajetan.august@gmail.com>
2025-07-30 23:18:34 +0200

    feat(Sitemap): add sitemap integration and robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,15 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
+import {defineConfig} from 'astro/config';
+import {vanillaExtractPlugin} from '@vanilla-extract/vite-plugin';
+import sitemap from '@astrojs/sitemap';
 
 
 // https://astro.build/config
 export default defineConfig({
+    site: 'https://jesien.com.pl',
+    integrations: [
+        sitemap(),
+    ],
     vite: {
         plugins: [vanillaExtractPlugin()]
     }
