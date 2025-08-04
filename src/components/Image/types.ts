1d9cd81fb566f431485f7e7c6be639600888eb0c
Kajetan August <kajetan.august@gmail.com>
2025-08-04 21:42:33 +0200

    feat(Image): refactor Image component

--- src/components/Image/types.ts
+++ src/components/Image/types.ts
@@ -1,4 +1,6 @@
 export type ObjectFit = 'cover' | 'contain' | 'fill' | 'scale-down' | 'none'
 export type ObjectPosition = 'top' | 'right' | 'bottom' | 'left' | 'center'
-export type AspectRatio = '1/1' | '4/3' | '3/4' | '16/9' | '9/16' | '21/9' | '3/2' | '2/3'
 export type ResponsiveType = 'fluid' | 'fixed' | 'fullWidth' | 'fullHeight'
+export type FetchPriority = 'auto' | 'high' | 'low'
+export type Layout = 'constrained' | 'full-width' | 'fixed' | 'none'
+export type Loading = 'eager' | 'lazy'
