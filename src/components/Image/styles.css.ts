4743520a6ad2ac058124d11257c09a4d3d9344ed
Kajetan August <kajetan.august@gmail.com>
2025-08-04 21:51:47 +0200

    fix(Image): fix image props

--- src/components/Image/styles.css.ts
+++ src/components/Image/styles.css.ts
@@ -3,7 +3,6 @@ import {style} from "@vanilla-extract/css";
 export const imageBase = style({
     width: '100%',
     height: 'auto',
-    display: 'block',
     maxWidth: '100%',
     maxHeight: '100%',
 })
