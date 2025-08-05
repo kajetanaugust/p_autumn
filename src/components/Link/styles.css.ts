67f1b37f3057412ea8a91e9a5a93d6f2f9bcd128
Kajetan August <kajetan.august@gmail.com>
2025-08-05 20:56:27 +0200

    feat(Link): add hover, active and focus-visible styles

--- src/components/Link/styles.css.ts
+++ src/components/Link/styles.css.ts
@@ -3,4 +3,17 @@ import {theme} from "../../styles/theme.css";
 
 export const styledLink = style({
     color: theme.colors.neutral_darker,
+
+    ':hover': {
+        color: `${theme.colors.luxor_gold_dark} !important`,
+    },
+
+    ':active': {
+        color: `${theme.colors.luxor_gold_darker} !important`,
+    },
+
+    ':focus-visible': {
+        outline: `2px solid ${theme.colors.luxor_gold_darker || '#0066cc'}`,
+        outlineOffset: '2px',
+    },
 })
