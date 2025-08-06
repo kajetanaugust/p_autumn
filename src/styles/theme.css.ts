0b3a0796bf6b9410ed4738d89f4b35c842e2bcd1
kajetanaugust <kajetan.august@gmail.com>
2025-08-06 13:52:43 +0200

    refactor(Theme): remove unused variables

--- src/styles/theme.css.ts
+++ src/styles/theme.css.ts
@@ -1,14 +1,12 @@
-import {createGlobalTheme} from "@vanilla-extract/css";
-import {colors} from "../tokens/Colors";
-import {radius} from "../tokens/Radius";
-import {shadows} from "../tokens/Shadows";
-import {breakpoints, gap, spacing} from "./vars.ts";
+import { createGlobalTheme } from '@vanilla-extract/css'
+import { colors } from '../tokens/Colors'
+import { radius } from '../tokens/Radius'
+import { shadows } from '../tokens/Shadows'
+import { breakpoints } from './vars.ts'
 
 export const theme = createGlobalTheme(':root', {
-    colors: colors,
-    radius: radius,
-    shadows: shadows,
-    breakpoints: breakpoints,
-    gap: gap,
-    spacing: spacing
+  colors: colors,
+  radius: radius,
+  shadows: shadows,
+  breakpoints: breakpoints,
 })
--- src/styles/vars.ts
+++ src/styles/vars.ts
@@ -1,38 +1,6 @@
 export const breakpoints = {
-    sm: '576px',
-    md: '768px',
-    lg: '992px',
-    xl: '1200px',
+  sm: '576px',
+  md: '768px',
+  lg: '992px',
+  xl: '1200px',
 }
-
-export const gap = {
-    2: '2px',
-    4: '4px',
-    8: '8px',
-    16: '16px',
-    24: '24px',
-    32: '32px',
-    48: '48px',
-    64: '64px',
-    80: '80px',
-    96: '96px',
-    128: '128px',
-}
-
-export const spacing = {
-    2: '2px',
-    4: '4px',
-    8: '8px',
-    16: '16px',
-    24: '24px',
-    32: '32px',
-    48: '48px',
-    64: '64px',
-    80: '80px',
-    96: '96px',
-    112: '112px',
-    128: '128px',
-
-}
-
-
