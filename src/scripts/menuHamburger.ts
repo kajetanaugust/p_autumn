19a4ab12af8854e3459b48b2405d021c95f97132
Kajetan August <kajetan.august@gmail.com>
2025-08-09 12:39:24 +0200

    fix(Header): add mobile menu closing on button click

--- src/scripts/menuHamburger.ts
+++ src/scripts/menuHamburger.ts
@@ -5,6 +5,15 @@ const mobileMenu = document.getElementById('mobile-menu')
 const html = document.querySelector('html')
 const body = document.querySelector('body')
 
+function closeMobileMenu() {
+  if (!mobileMenu || !hamburger || !html || !body) return
+
+  mobileMenu.classList.remove(mobileMenuVisible)
+  hamburger.setAttribute('aria-expanded', 'false')
+  html.style.overflowY = 'auto'
+  body.style.overflowY = 'auto'
+}
+
 hamburger?.addEventListener('click', () => {
   mobileMenu?.classList.toggle(mobileMenuVisible)
   const isMenuVisible = mobileMenu?.classList.contains(mobileMenuVisible)
@@ -15,4 +24,13 @@ hamburger?.addEventListener('click', () => {
 
   html.style.overflowY = isMenuVisible ? 'hidden' : 'auto'
   body.style.overflowY = isMenuVisible ? 'hidden' : 'auto'
+})
+
+mobileMenu?.addEventListener('click', event => {
+  const target = event.target as HTMLElement
+  const clickedButton = target.closest('a[href]')
+
+  if (clickedButton) {
+    closeMobileMenu()
+  }
 })
