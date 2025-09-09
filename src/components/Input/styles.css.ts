614ce7bdde8077027a0d4ff35713118425ce30c8
kajetanaugust <kajetan.august@gmail.com>
2025-09-09 14:58:26 +0200

    feat(Input): add input styling, change copy of inputs in contact modal

--- src/components/Input/styles.css.ts
+++ src/components/Input/styles.css.ts
@@ -1,8 +1,17 @@
 import { style } from '@vanilla-extract/css'
+import { theme } from '@styles/theme.css.ts'
 
 export const styledInput = style({
   height: '48px',
   width: '100%',
   border: 'none',
-  borderBottom: '1px solid #000',
+  borderBottom: `1px solid ${theme.colors.neutral_lighter}`,
+  selectors: {
+    '&:placeholder': {
+      color: theme.colors.neutral_light,
+      fontSize: '1rem',
+      fontWeight: 300,
+      lineHeight: 1.5,
+    },
+  },
 })
