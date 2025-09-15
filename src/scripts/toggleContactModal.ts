8bed3288921814c047918f18ed9a96eedbec7030
Kajetan August <kajetan.august@gmail.com>
2025-09-15 20:06:04 +0200

    feat(contactForm): add form handling with netlify, add captcha

--- src/scripts/toggleContactModal.ts
+++ src/scripts/toggleContactModal.ts
@@ -1,5 +1,7 @@
 import { modalHidden, modalVisible } from '@components/Modal/styles.css.ts'
+import { mobileMenuVisible } from '@sections/Header/styles.css.ts'
 
+const mobileMenu = document.getElementById('mobile-menu')
 const contactModal = document.getElementById('contact-modal')
 const contactButton = document.querySelectorAll(
   '[data-trigger="contact-modal"]'
@@ -11,9 +13,13 @@ const toggleModal = (modal: HTMLElement): void => {
   if (isCurrentlyVisible) {
     modal.classList.remove(modalVisible)
     modal.classList.add(modalHidden)
+    history.pushState(null, '', '/')
   } else {
     modal.classList.remove(modalHidden)
     modal.classList.add(modalVisible)
+    mobileMenu?.classList.remove(mobileMenuVisible)
+
+    history.pushState(null, '', '#formularz-kontaktowy')
   }
 }
 
---
+++ src/sections/ContactModal/styles.css.ts
@@ -0,0 +1,10 @@
+import { style } from '@vanilla-extract/css'
+
+export const styledContactForm = style({
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '24px',
+  alignItems: 'center',
+  height: 'fit-content',
+  width: '100%',
+})
